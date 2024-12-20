9c7c3cd3bf5638a547127efd183e5b94d4895251
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2024-12-19 22:34:50 -0500

    Set Up Radix UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "./NavBar";
+import { Theme } from "@radix-ui/themes";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
