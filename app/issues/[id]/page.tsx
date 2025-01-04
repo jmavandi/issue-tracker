93bfa4cadb441a9c6940e254aec6b1909fec1305
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2025-01-04 11:03:46 -0500

    Secure the application

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,12 +4,16 @@ import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 interface Props {
   params: Promise<{ id: string }>;
 }
 
 const IssueDetailPage = async (props: Props) => {
+  const session = await getServerSession(authOptions);
+
   const params = await props.params;
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -22,12 +26,14 @@ const IssueDetailPage = async (props: Props) => {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction="column" gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
